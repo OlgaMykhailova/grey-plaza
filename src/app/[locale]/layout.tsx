3e063aae595405cd58fed45530cbe8322f538d67
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-04-17 19:22:34 +0300

    add next-intl

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,31 @@
+import { getTranslations } from "next-intl/server";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const t = await getTranslations({ locale, namespace: "Metadata" });
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
+
+export default function LocaleLayout({
+  children,
+  params: { locale },
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: string };
+}>) {
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
