93ccc808924ca6f8d1ec1a1e8feaf1ce37494708
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-04-30 23:20:33 +0300

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import { getTranslations } from "next-intl/server";
 import { Inter } from "next/font/google";
+import { NextIntlClientProvider, useMessages } from "next-intl";
 import Header from "@/src/components/shared/header/Header";
 import Footer from "@/src/components/shared/footer/Footer";
 import "./globals.css";
@@ -25,13 +26,17 @@ export default function LocaleLayout({
   children: React.ReactNode;
   params: { locale: string };
 }>) {
+  const messages = useMessages();
+
   return (
     <html lang={locale} suppressHydrationWarning>
-      <body className={`${inter.className} bg-white-bg`}>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </body>
+      <NextIntlClientProvider locale={locale} messages={messages}>
+        <body className={`${inter.className} bg-white-bg`}>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </NextIntlClientProvider>
     </html>
   );
 }
