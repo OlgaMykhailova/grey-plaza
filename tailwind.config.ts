dd34b1494c4efa8b64d4b364c08da6c47201c9b9
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-04-30 17:42:18 +0300

    added font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,64 @@ const config: Config = {
         tablet: "768px",
         desktop: "1280px",
       },
+      fontSize: {
+        xs: [
+          "14px",
+          {
+            lineHeight: "18px",
+            fontWeight: "400",
+          },
+        ],
+        xsb: [
+          "14px",
+          {
+            lineHeight: "18px",
+            fontWeight: "700",
+          },
+        ],
+        base: [
+          "16px",
+          {
+            lineHeight: "21px",
+            fontWeight: "400",
+          },
+        ],
+        baseb: [
+          "16px",
+          {
+            lineHeight: "24px",
+            fontWeight: "700",
+          },
+        ],
+        mdb: [
+          "18px",
+          {
+            lineHeight: "24px",
+            fontWeight: "700",
+          },
+        ],
+        lgb: [
+          "20px",
+          {
+            lineHeight: "28px",
+            fontWeight: "700",
+          },
+        ],
+        xlsb: [
+          "28px",
+          {
+            lineHeight: "32px",
+            fontWeight: "600",
+          },
+        ],
+        xxlb: [
+          "44px",
+          {
+            lineHeight: "48px",
+            fontWeight: "700",
+          },
+        ],
+      },
     },
   },
   plugins: [],
