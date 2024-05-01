78c1825d895cc125f235e080b0066abf0cf1e5da
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-01 12:17:32 +0300

    added home page file structure

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,25 @@
+import Hero from "@/src/components/home/hero/Hero";
 import Offers from "@/src/components/home/offers/Offers";
+import Infrastructure from "@/src/components/home/infrastructure/Infrastructure";
+import Equipment from "@/src/components/home/equipment/Equipment";
+import Benefits from "@/src/components/home/benefits/Benefits";
+import Offices from "@/src/components/home/offices/Offices";
+import Gallery from "@/src/components/home/gallery/Gallery";
+import WriteUs from "@/src/components/shared/writeUs/WriteUs";
+import Location from "@/src/components/home/location/Location";
 
 export default function Home() {
   return (
     <>
+      <Hero />
       <Offers />
+      <Infrastructure />
+      <Equipment />
+      <Benefits />
+      <Offices />
+      <Gallery />
+      <WriteUs />
+      <Location />
     </>
   );
 }
