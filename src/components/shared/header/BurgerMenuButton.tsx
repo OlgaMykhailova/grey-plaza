648f0bbb80fa07506179eabe9611d0dffeceaa06
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-03 22:19:11 +0300

    update burger menu

--- src/components/shared/header/BurgerMenuButton.tsx
+++ src/components/shared/header/BurgerMenuButton.tsx
@@ -2,12 +2,12 @@ interface BurgerMenuButtonProps {
   isHeaderMenuOpened?: boolean;
   toggleHeaderMenuOpen?: () => void;
 }
-const firstLineBaseStyles = "top-[2px] left-0 opacity-100";
+const firstLineBaseStyles = "top-[3px] left-0 opacity-100";
 const firstLineTransformStyles = "top-[-2px] left-[3px] opacity-0";
-const secondLineBaseStyles = "top-[8px] left-0";
-const secondLineTransformStyles = "rotate-[-45deg] top-[7px]";
-const thirdLineBaseStyles = "top-[14px] left-0";
-const thirdLineTransformStyles = "rotate-45 top-[7px]";
+const secondLineBaseStyles = "top-[11px] left-0";
+const secondLineTransformStyles = "rotate-[-45deg] top-[11px]";
+const thirdLineBaseStyles = "top-[19px] left-0";
+const thirdLineTransformStyles = "rotate-45 top-[11px]";
 
 export default function BurgerMenuButton({
   isHeaderMenuOpened,
@@ -18,18 +18,18 @@ export default function BurgerMenuButton({
       aria-label="open menu button"
       type="button"
       onClick={toggleHeaderMenuOpen}
-      className="group relative w-10 h-10 p-3 outline-none"
+      className="group relative w-10 h-10 p-2 outline-none"
     >
       <div className="w-full h-full rotate-0">
         <span
           className={`block absolute w-full h-[1.8px] rounded-md bg-white-text rotate-0 
-          transition duration-[600ms] ease-out-quart origin-right-center ${
+          transition duration-[600ms] ease-out-quart ${
             isHeaderMenuOpened ? firstLineTransformStyles : firstLineBaseStyles
           }`}
         ></span>
         <span
           className={`block absolute left-0 w-full h-[1.8px] rounded-md bg-white-text rotate-0 
-          transition duration-[600ms] ease-out-quart origin-right-center ${
+          transition duration-[600ms] ease-out-quart ${
             isHeaderMenuOpened
               ? secondLineTransformStyles
               : secondLineBaseStyles
@@ -37,7 +37,7 @@ export default function BurgerMenuButton({
         ></span>
         <span
           className={`block absolute left-0 w-full h-[1.8px] rounded-md bg-white-text rotate-0  
-          transition duration-[600ms] ease-out-quart origin-right-center ${
+          transition duration-[600ms] ease-out-quart ${
             isHeaderMenuOpened ? thirdLineTransformStyles : thirdLineBaseStyles
           }`}
         ></span>
