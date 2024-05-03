d7aabd73d7b51d9021d3efbae8f040d62d428383
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-03 22:09:57 +0300

    update offers marquee

--- src/components/home/offers/Offers.tsx
+++ src/components/home/offers/Offers.tsx
@@ -10,25 +10,25 @@ export default function Offers() {
 
   return (
     <section>
-      <Marquee
-        autoFill={true}
-        pauseOnHover={true}
-        pauseOnClick={true}
-        className="h-[72px] bg-accent"
-      >
-        <h2 className="flex items-center text-lgb text-white-text">
-          {t("Offers.title")}
-          <span className="text-3xs">
-            &nbsp; &nbsp; &nbsp; ⬢ &nbsp; &nbsp; &nbsp;
-          </span>
-          <Link href={`/${locale}#contacts`}>
-            <Button variant="secondary">{t("Buttons.order")}</Button>
-          </Link>
-          <span className="text-3xs">
-            &nbsp; &nbsp; &nbsp; ⬢ &nbsp; &nbsp; &nbsp;
-          </span>
-        </h2>
-      </Marquee>
+      <Link href={`/${locale}#contacts`}>
+        <Marquee
+          autoFill={true}
+          pauseOnHover={true}
+          pauseOnClick={true}
+          className="h-[72px] bg-accent"
+        >
+          <h2 className="flex items-center text-lgb text-white-text">
+            {t("Offers.title")}
+            <span className="text-3xs">
+              &nbsp; &nbsp; &nbsp; ⬢ &nbsp; &nbsp;
+            </span>
+
+            <span className="text-3xs">
+              &nbsp; &nbsp; ⬢ &nbsp; &nbsp; &nbsp;
+            </span>
+          </h2>
+        </Marquee>
+      </Link>
     </section>
   );
 }
