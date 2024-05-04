9e4282cb2abf97b63ef06f8486767716e76826fb
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-04 12:07:49 +0300

    Update HeaderMenu.tsx

--- src/components/shared/header/HeaderMenu.tsx
+++ src/components/shared/header/HeaderMenu.tsx
@@ -22,10 +22,10 @@ export default function HeaderMenu({
   return (
     <div
       className={`absolute top-[72px] left-0 z-20 w-[100vw] overflow-hidden transition-[height] duration-[600ms] ease-out-quart bg-primary bg-opacity-[0.8] ${
-        isHeaderMenuOpened ? "h-[calc(100dvh-72px)]" : "h-0"
+        isHeaderMenuOpened ? "min-h-[calc(100dvh-72px)]" : "h-0"
       }`}
     >
-      <Container className="flex flex-col items-center gap-y-10 h-full py-10">
+      <Container className="flex flex-col items-center gap-y-10 py-10">
         <HeaderNavigationMenu onClick={() => setIsHeaderMenuOpened(false)} />
         <SocialLinks />
         <Link href={`/${locale}#write-us`} className="outline-none">
