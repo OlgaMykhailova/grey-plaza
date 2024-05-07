04b5175ea5fc6c141b3c3c3c5c553e1345298274
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-07 22:19:59 +0300

    updated map component

--- src/components/home/location/Map.tsx
+++ src/components/home/location/Map.tsx
@@ -1,31 +1,50 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
+import TextButton from "../../shared/TextButton";
+import Container from "../../shared/Container";
+
+const MAP_URL = "https://maps.app.goo.gl/XdyPTk5aaQVUhvH78";
 
 export default function Map() {
+  const t = useTranslations("Buttons");
+
   return (
-    <div className="relative">
-      <Image
-        src={`/images/map.jpg`}
-        width="0"
-        height="0"
-        sizes="100%"
-        alt="map"
-        className="w-full h-auto"
-      />
-      <a
-        href="https://maps.app.goo.gl/XdyPTk5aaQVUhvH78"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="outline-none"
-      >
+    <>
+      <Container className="flex justify-center">
+        <a
+          href={MAP_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="outline-none"
+        >
+          <TextButton className="mb-5">{t("route")}</TextButton>{" "}
+        </a>
+      </Container>
+      <div className="relative">
         <Image
-          src={`/images/icons/locationLarge.svg`}
+          src={`/images/map.jpg`}
           width="0"
           height="0"
           sizes="100%"
-          alt="location icon"
-          className="absolute top-[36%] right-[26%] w-[19%] h-auto"
+          alt="map"
+          className="w-full h-auto"
         />
-      </a>
-    </div>
+        <a
+          href={MAP_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="outline-none"
+        >
+          <Image
+            src={`/images/icons/locationLarge.svg`}
+            width="0"
+            height="0"
+            sizes="100%"
+            alt="location icon"
+            className="absolute top-[36%] right-[26%] w-[19%] h-auto"
+          />
+        </a>
+      </div>
+    </>
   );
 }
