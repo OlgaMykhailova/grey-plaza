f51504847823721f7dedffc963503c671953b9b7
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-08 15:03:50 +0300

    updated hexagons bg position

--- src/components/shared/SectionHexagonBgImage.tsx
+++ src/components/shared/SectionHexagonBgImage.tsx
@@ -7,7 +7,7 @@ export default function SectionHexagonBgImage() {
       width="0"
       height="0"
       alt="hexagons background"
-      className="absolute top-0 left-0 z-[-10] min-w-[313px] max-w-[414px] w-[84%] mobileXl:min-w-[414px] mobileXl:max-w-[514px] mobileXl:w-[70%] h-auto"
+      className="absolute top-0 left-[calc((100%-313px)/2)] z-[-10] w-[313px] h-auto"
     />
   );
 }
--- src/components/shared/TextButton.tsx
+++ src/components/shared/TextButton.tsx
@@ -9,7 +9,7 @@ export default function TextButton({ children, className }: TextButtonProps) {
   return (
     <button
       type="button"
-      className={`w-[320px] h-10 border-y-[1px] border-primary ${className}`}
+      className={`w-[320px] h-10 border-y border-primary ${className}`}
     >
       {children}
     </button>
