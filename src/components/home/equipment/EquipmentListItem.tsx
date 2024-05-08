fbae71f2e49d92c60d2db628ac0c98840ed667aa
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-08 16:58:26 +0300

    changes alt images for equipment section

--- src/components/home/equipment/EquipmentListItem.tsx
+++ src/components/home/equipment/EquipmentListItem.tsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
 
 interface EquipmentListItemProps {
-  equipment: { title: string; icon: string };
+  equipment: { title: string; icon: string; alt: string };
 }
 
 export default function EquipmentListItem({
   equipment,
 }: EquipmentListItemProps) {
-  const { title, icon } = equipment;
+  const { title, icon, alt } = equipment;
   return (
     <li className="flex flex-col items-center justify-center gap-4 p-4 [&:not(:last-child)]:mb-2 after::content-[''] after:w-[185px] after:h-[1px] after:bg-accent">
       <Image
         src={`/images/icons/${icon}`}
         width="0"
         height="0"
-        alt={title}
+        alt={alt}
         className="w-[64px] h-[64px]"
       />
       <h3 className="text-mdb text-center">{title}</h3>
