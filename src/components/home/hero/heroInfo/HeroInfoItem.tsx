ec86e553b7ce0569f2dcf97c5a2f17d93cf7cf0d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-11 10:18:54 +0300

    added heroInfo, made refactoring

--- src/components/home/hero/heroInfo/HeroInfoItem.tsx
+++ src/components/home/hero/heroInfo/HeroInfoItem.tsx
@@ -7,13 +7,13 @@ interface HeroInfoItemProps {
 export default function HeroInfoItem({ heroInfoItem }: HeroInfoItemProps) {
   const { quantity, description } = heroInfoItem;
   return (
-    <li className="absolute flex flex-col items-center justify-center w-[88px] h-[102px]">
+    <li className="absolute first:left-[5.6%] first:top-[34.4%] [&:nth-child(2)]:left-[24.4%] [&:nth-child(2)]:top-[24.5%] [&:nth-child(3)]:right-[24.4%] [&:nth-child(3)]:top-[24.5%] last:top-[34.4%] last:right-[5.6%] flex flex-col items-center justify-center min-w-[88px] w-[24.4%] min-h-[102px] h-[15%]">
       <Image
         src={`/images/icons/hexagon.svg`}
         width="0"
         height="0"
         alt="hexagone image"
-        className="absolute top-0 left-0 w-[88px] h-[102px]"
+        className="absolute top-0 left-0 w-full h-full"
       />
       <span className="mb-2 text-xsb">{quantity}</span>
       <span className="text-xs">{description}</span>
