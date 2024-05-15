5008b41243814ee9be2259132c82318f6125e714
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-15 23:07:34 +0300

    added write us section to the offices page

--- src/app/[locale]/offices/page.tsx
+++ src/app/[locale]/offices/page.tsx
@@ -1,3 +1,9 @@
+import WriteUs from "@/src/components/shared/writeUs/WriteUs";
+
 export default function Offices() {
-  return <></>;
+  return (
+    <div className="pt-[72px]">
+      <WriteUs />
+    </div>
+  );
 }
