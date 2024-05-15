c024f2a9da2c0d7769398916f45b9b3a635b67e6
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-15 23:30:26 +0300

    added hero to the rent page without photo

--- src/app/[locale]/rent/page.tsx
+++ src/app/[locale]/rent/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/src/components/rent/hero/Hero";
 import WriteUs from "@/src/components/shared/writeUs/WriteUs";
 
 export default function Offices() {
   return (
     <div className="pt-[72px]">
+      <Hero />
       <WriteUs />
     </div>
   );
---
+++ src/components/rent/hero/Hero.tsx
@@ -0,0 +1,9 @@
+import HeroTitle from "./HeroTitle";
+
+export default function Hero() {
+  return (
+    <section className="flex justify-center items-center w-full min-h-[156px] bg-primary">
+      <HeroTitle />
+    </section>
+  );
+}
