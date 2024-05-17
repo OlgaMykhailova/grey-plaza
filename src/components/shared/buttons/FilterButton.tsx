2df94f656603f362585c5aa788fdd9cd4d953584
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-17 19:02:37 +0300

    added changing color to the active filter button

--- src/components/shared/buttons/FilterButton.tsx
+++ src/components/shared/buttons/FilterButton.tsx
@@ -1,23 +1,22 @@
-"use client";
-import { useState } from "react";
-
 interface FilterButtonProps {
   children: string;
   className?: string;
+  currentCategory?: string;
+  handleCLick?: () => void;
 }
 
 export default function FilterButton({
   children,
   className = "",
+  handleCLick,
+  currentCategory,
 }: FilterButtonProps) {
-  const [isChoosed, setIsChoosed] = useState(false);
-
   return (
     <button
       type="button"
-      onClick={() => setIsChoosed(true)}
+      onClick={handleCLick}
       className={`flex items-center justify-center w-full h-10 text-baseb border ${
-        isChoosed
+        currentCategory === children
           ? "text-white-bgSecondary bg-primary"
           : "text-primary border-primary bg-white-bgSecondary"
       } ${className}`}
