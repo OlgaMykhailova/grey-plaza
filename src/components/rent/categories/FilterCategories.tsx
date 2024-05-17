a354b9b4d6f845199346b0c20243cfad8859bffe
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-17 19:36:07 +0300

    added choose another category button

--- src/components/rent/categories/FilterCategories.tsx
+++ src/components/rent/categories/FilterCategories.tsx
@@ -1,15 +1,23 @@
-import { useTranslations } from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
+import Link from "next/link";
 import Container from "../../shared/Container";
 import FilterButtonsList from "./FilterButtonsList";
+import TextButton from "../../shared/buttons/TextButton";
 
 export default function FilterCategories() {
   const t = useTranslations("Rent");
+  const locale = useLocale();
 
   return (
-    <section className="py-10">
-      <Container>
-        <h2 className="mb-3 text-black text-lgb">{t("chooseCategory")}</h2>
-        <FilterButtonsList />
+    <section id="categories" className="py-10 scroll-mt-[72px]">
+      <Container className="flex flex-col items-center">
+        <div>
+          <h2 className="mb-3 text-black text-lgb">{t("chooseCategory")}</h2>
+          <FilterButtonsList />
+        </div>
+        <Link href={`/${locale}/rent#categories`}>
+          <TextButton className="mt-8">{t("chooseAnotherCategory")}</TextButton>
+        </Link>
       </Container>
     </section>
   );
