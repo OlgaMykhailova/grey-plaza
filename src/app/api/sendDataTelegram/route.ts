d5f0665e36000faba059366b78be71e46c540130
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-21 18:55:55 +0300

    added send data api route

---
+++ src/app/api/sendDataTelegram/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+
+const URL_API = process.env.NEXT_PUBLIC_TELEGRAM_URL_API || "";
+
+export async function POST(request: NextRequest) {
+  try {
+    const data = await request.json();
+    let message = `<b>Нове замовлення</b>\n`;
+    message +=
+      `Ім'я: ${data?.name}\n` +
+      `Телефон: ${data?.phone}\n` +
+      `Email: ${data?.email}\n` +
+      `Повідомлення: ${data?.message}\n`;
+
+    axios.post(URL_API, {
+      chat_id: process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID,
+      parse_mode: "html",
+      text: message,
+    });
+    return NextResponse.json({ message: "Data sent successfully" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to send data to the telegram" },
+      { status: 500 }
+    );
+  }
+}
