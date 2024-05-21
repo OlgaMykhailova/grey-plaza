f9432d25a1f7c61100480ffd61759aaa5c2d897c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-21 22:40:24 +0300

    Update route.ts

--- src/app/api/sendDataTelegram/route.ts
+++ src/app/api/sendDataTelegram/route.ts
@@ -4,26 +4,27 @@ import axios from "axios";
 const URL_API = process.env.NEXT_PUBLIC_TELEGRAM_URL_API || "";
 
 export async function POST(request: NextRequest) {
-  try {
-    const data = await request.json();
-    let message = `<b>Нове замовлення</b>\n`;
-    message +=
-      `Ім'я: ${data?.name}\n` +
-      `Телефон: ${data?.phone}\n` +
-      `Email: ${data?.email}\n` +
-      `Повідомлення: ${data?.message}\n`;
+  if (request.method === "POST") {
+    try {
+      const data = await request.json();
+      let message = `<b>Нова заявка</b>\n`;
+      message +=
+        `Ім'я: ${data?.name}\n` +
+        `Телефон: ${data?.phone}\n` +
+        `Email: ${data?.email}\n` +
+        `Повідомлення: ${data?.message}\n`;
 
-    await axios.post(URL_API, {
-      chat_id: process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID,
-      parse_mode: "html",
-      text: message,
-    });
-    return NextResponse.json({ message: "Data sent successfully" });
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to append data to the sheet" },
-      { status: 500 }
-    );
-    return error;
+      await axios.post(URL_API, {
+        chat_id: process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID,
+        parse_mode: "html",
+        text: message,
+      });
+      return NextResponse.json({ message: "Data sent successfully" });
+    } catch (error) {
+      return NextResponse.json(
+        { error: "Failed to append data to the sheet" },
+        { status: 500 }
+      );
+    }
   }
 }
