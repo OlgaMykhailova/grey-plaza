992a56e0b6972ebc0fbc90469a23393d73b2dc98
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-23 13:52:24 +0300

    updated header styles for tablet (#60)

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -12,10 +12,10 @@ export default function Header() {
   const toggleHeaderMenuOpen = () => setIsHeaderMenuOpened(!isHeaderMenuOpened);
 
   return (
-    <header className="fixed top-0 left-0 z-20 w-[100vw] h-[72px] bg-blue">
-      <Container className="flex justify-between items-center px-5 py-4">
-        <LogoLink className="w-[78px] h-[40px]" />
-        <div className="flex items-center gap-y-5">
+    <header className="fixed top-0 left-0 z-20 w-[100vw] h-[72px] tablet:h-20 bg-blue">
+      <Container className="flex justify-between items-center h-full">
+        <LogoLink className="w-[78px] tablet:w-[110px] h-10 tablet:h-14" />
+        <div className="flex items-center tablet:gap-x-5">
           <PhoneLink />
           <BurgerMenuButton
             isHeaderMenuOpened={isHeaderMenuOpened}
