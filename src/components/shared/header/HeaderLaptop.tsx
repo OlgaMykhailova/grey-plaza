5ffa52540c9f901f5773ea37d4d90a994a94e273
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-27 23:08:12 +0300

    added logo link

--- src/components/shared/header/HeaderLaptop.tsx
+++ src/components/shared/header/HeaderLaptop.tsx
@@ -1,5 +1,12 @@
+import Container from "../Container";
+import LogoLink from "../LogoLink";
+
 export default function HeaderLaptop() {
   return (
-    <header className="hidden laptop:block fixed top-0 left-0 z-20 w-[100vw] h-20 bg-blue"></header>
+    <header className="hidden laptop:block fixed top-0 left-0 z-20 w-[100vw] h-20 bg-blue">
+      <Container className="flex justify-between items-center h-full">
+        <LogoLink className="w-[110px] h-14" />
+      </Container>
+    </header>
   );
 }
