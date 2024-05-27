edeb4a6ed965f8de24ece45a9ad0d789b9e429be
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-27 14:16:47 +0300

    updated location section styles for tablet

--- src/components/home/location/Location.tsx
+++ src/components/home/location/Location.tsx
@@ -4,19 +4,34 @@ import SectionTitle from "../../shared/SectionTitle";
 import LocationList from "./LocationList";
 import SectionHexagonBgImage from "../../shared/SectionHexagonBgImage";
 import Map from "./Map";
+import TextButton from "../../shared/buttons/TextButton";
+
+const MAP_URL = "https://maps.app.goo.gl/XdyPTk5aaQVUhvH78";
 
 export default function Location() {
-  const t = useTranslations("Location");
+  const t = useTranslations("");
 
   return (
-    <section className="relative z-10 pt-[80px] mb-10 scroll-mt-[72px]">
+    <section className="relative z-10 pt-20 tablet:pt-10 mb-10 scroll-mt-[72px]">
       <SectionHexagonBgImage />
-      <Container className="flex flex-col items-center">
-        <SectionTitle>{t("title")}</SectionTitle>
-        <p className="mb-5 text-base">{t("description")}</p>
-        <LocationList />
-      </Container>
-      <Map />
+      <SectionTitle className="w-1/2 mx-auto">
+        {t("Location.title")}
+      </SectionTitle>
+      <div className="flex flex-col tablet:flex-row items-center justify-between">
+        <Container>
+          <p className="mb-5 text-base">{t("Location.description")}</p>
+          <LocationList />
+          <a
+            href={MAP_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center justify-center mb-5 tablet:mb-0 outline-none"
+          >
+            <TextButton>{t("Buttons.route")}</TextButton>
+          </a>
+        </Container>
+        <Map />
+      </div>
     </section>
   );
 }
