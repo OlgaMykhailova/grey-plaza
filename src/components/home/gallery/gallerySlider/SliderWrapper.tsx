8a51795f45d52d299b5c9dd6d9fa1ad80d8a7122
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-27 15:56:28 +0300

    updated gallery slider

--- src/components/home/gallery/gallerySlider/SliderWrapper.tsx
+++ src/components/home/gallery/gallerySlider/SliderWrapper.tsx
@@ -14,20 +14,25 @@ export default function SliderWrapper({ children }: SliderWrapperProps) {
   return (
     <Swiper
       modules={[Scrollbar, EffectCoverflow]}
-      slidesPerView={1.35}
+      slidesPerView={1.3}
       scrollbar={true}
       grabCursor={true}
       centeredSlides={true}
       effect={"coverflow"}
       coverflowEffect={{
         rotate: 0,
-        scale: 0.75,
+        scale: 0.7,
         stretch: 32,
         depth: 0,
         modifier: 1,
         slideShadows: false,
       }}
-      breakpoints={{ 768: { slidesPerView: 1.25 } }}
+      breakpoints={{
+        768: {
+          slidesPerView: 1.12,
+          coverflowEffect: { scale: 0.75, stretch: 72 },
+        },
+      }}
       speed={1000}
       loop={true}
       cssMode={true}
