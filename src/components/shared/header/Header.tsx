0ad9e35666cfc4cfcb7e1a588b75b2baef870bd1
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-28 13:25:37 +0300

    updated header for laptop

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,11 +1,32 @@
-import HeaderLaptop from "./HeaderLaptop";
-import HeaderMobTab from "./HeaderMobTab";
+"use client";
+import { useState } from "react";
+import Container from "../Container";
+import LogoLink from "../LogoLink";
+import BurgerMenuButton from "./BurgerMenuButton";
+import PhoneLink from "./PhoneLink";
+import HeaderMenu from "./HeaderMenu";
 
 export default function Header() {
+  const [isHeaderMenuOpened, setIsHeaderMenuOpened] = useState(false);
+
+  const toggleHeaderMenuOpen = () => setIsHeaderMenuOpened(!isHeaderMenuOpened);
+
   return (
-    <>
-      <HeaderMobTab />
-      {/* <HeaderLaptop /> */}
-    </>
+    <header className="fixed top-0 left-0 z-20 w-[100vw] h-[72px] tablet:h-20 bg-blue">
+      <Container className="flex justify-between items-center h-full">
+        <LogoLink className="w-[78px] tablet:w-[110px] h-10 tablet:h-14" />
+        <div className="flex items-center tablet:gap-x-5">
+          <PhoneLink />
+          <BurgerMenuButton
+            isHeaderMenuOpened={isHeaderMenuOpened}
+            toggleHeaderMenuOpen={toggleHeaderMenuOpen}
+          />
+        </div>
+      </Container>
+      <HeaderMenu
+        isHeaderMenuOpened={isHeaderMenuOpened}
+        setIsHeaderMenuOpened={setIsHeaderMenuOpened}
+      />
+    </header>
   );
 }
--- src/components/shared/header/HeaderMenu.tsx
+++ src/components/shared/header/HeaderMenu.tsx
@@ -16,11 +16,11 @@ export default function HeaderMenu({
 }: HeaderMenuProps) {
   return (
     <div
-      className={`absolute top-[72px] left-0 z-20 w-[100vw] overflow-hidden transition-[height] duration-[600ms] ease-out-quart bg-headerGradient ${
+      className={`absolute top-[72px] left-0 z-20 w-[100vw] overflow-hidden transition-[height] duration-[600ms] ease-out-quart bg-headerGradient backdrop-blur-sm ${
         isHeaderMenuOpened ? "h-[calc(100vh-72px)] no-doc-scroll" : "h-0"
       }`}
     >
-      <Container className="flex flex-col items-center gap-y-10 h-full py-10 overflow-y-auto">
+      <Container className="flex flex-col items-center gap-y-10 h-full py-10">
         <HeaderNavigationMenu onClick={() => setIsHeaderMenuOpened(false)} />
         <SocialLinks />
         <CallBack closeHeaderMenu={() => setIsHeaderMenuOpened(false)} />
