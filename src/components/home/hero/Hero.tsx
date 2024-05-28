a36645eae1ef4b6023cc92cc1df5b71e32f2804f
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-28 17:42:53 +0300

    added laptop hero images list

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <section
       id="about-us"
-      className="relative w-full aspect-[360/508] mobileXl:aspect-[768/660] scroll-mt-[72px] mb-10 bg-primary"
+      className="relative w-full aspect-[360/508] mobileXl:aspect-[768/660] laptop:aspect-[1280/876] scroll-mt-[72px] tablet:scroll-mt-20 mb-10 bg-primary"
     >
       <div className="absolute z-10 w-full h-full py-[11.11%] mobileXl:py-[8%] tablet:py-[12.1%] px-5 text-white-text text-center">
         <HeroTitle />
