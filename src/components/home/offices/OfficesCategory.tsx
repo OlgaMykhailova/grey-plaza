8e6aeffca02437f5606fd4172ef657d7e24e697b
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-29 21:43:05 +0300

    updated offices section styles for laptop

--- src/components/home/offices/OfficesCategory.tsx
+++ src/components/home/offices/OfficesCategory.tsx
@@ -14,7 +14,7 @@ export default function OfficesCategory({
   categoryName,
 }: OfficesCategoryProps) {
   return (
-    <div className="tablet:w-[48.9%] tablet:aspect-[356/320]">
+    <div className="tablet:w-[48.9%] laptop:w-[32%] tablet:aspect-[356/320] laptop:aspect-[390/347]">
       <OfficesSlider sliderList={sliderList} />
       <FilterButton className="tablet:h-[12.5%] mt-[3.43%]">
         {categoryName}
