388a80dfcfb3ff9c36a942e27c76143852a96e4d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-31 21:03:01 +0300

    updated email styles

--- src/components/policy/Section6.tsx
+++ src/components/policy/Section6.tsx
@@ -8,7 +8,18 @@ export default function Section6() {
     <li className="mb-4 laptop:mb-6">
       <SectionTitle>{t("policyChanges")}</SectionTitle>
       <ul>
-        <li>{t("policyChangesDescription")}</li>
+        <li>
+          {t("policyChangesDescription")}
+          <a
+            href={`mailto:${t("email")}`}
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            className="outline-none laptop:hover:text-accent laptop:focus-visible:text-accent transition-[color] duration-300 ease-out-quart"
+          >
+            {t("email")}
+          </a>
+          .
+        </li>
       </ul>
     </li>
   );
