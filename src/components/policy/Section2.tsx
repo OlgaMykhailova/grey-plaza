f28272bc707ef0c14ba69c27fe5b45584b3e0c13
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-31 20:40:02 +0300

    updated section2 styles

--- src/components/policy/Section2.tsx
+++ src/components/policy/Section2.tsx
@@ -1,31 +1,33 @@
 import { useTranslations } from "next-intl";
+import SectionTitle from "./SectionTitle";
+import SectionSubTitle from "./SectionSubTitle";
 
 export default function Section2() {
   const t = useTranslations("Policy");
 
   return (
-    <li>
-      <h2>{t("collectionPersonalData")}</h2>
-      <h3>{t("types")}</h3>
-      <ul>
+    <li className="mb-4 laptop:mb-6">
+      <SectionTitle>{t("collectionPersonalData")}</SectionTitle>
+      <SectionSubTitle>{t("types")}</SectionSubTitle>
+      <ul className="mb-3 laptop:mb-4">
         <li>{t("names")}</li>
         <li>{t("technicalInformation")}</li>
       </ul>
-      <h3>{t("collectionGoals")}</h3>
-      <ul>
+      <SectionSubTitle>{t("collectionGoals")}</SectionSubTitle>
+      <ul className="mb-3 laptop:mb-4">
         <li>{t("services")}</li>
         <li>{t("improvements")}</li>
         <li>{t("marketing")}</li>
       </ul>
-      <h3>{t("cookiesUsage")}</h3>
-      <ul>
+      <SectionSubTitle>{t("cookiesUsage")}</SectionSubTitle>
+      <ul className="mb-3 laptop:mb-4">
         <li>{t("cookies")}</li>
       </ul>
-      <h3>{t("period")}</h3>
-      <ul>
+      <SectionSubTitle>{t("period")}</SectionSubTitle>
+      <ul className="mb-3 laptop:mb-4">
         <li>{t("periodDescription")}</li>
       </ul>
-      <h3>{t("legalGrounds")}</h3>
+      <SectionSubTitle>{t("legalGrounds")}</SectionSubTitle>
       <ul>
         <li>{t("legalDescription")}</li>
       </ul>
