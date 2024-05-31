68f4601c5128ccc48f0e356e2eb987a01a3c5011
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-31 20:51:26 +0300

    updated section4 styles

--- src/components/policy/Section4.tsx
+++ src/components/policy/Section4.tsx
@@ -1,12 +1,13 @@
 import { useTranslations } from "next-intl";
+import SectionTitle from "./SectionTitle";
 
 export default function Section4() {
   const t = useTranslations("Policy");
 
   return (
-    <li>
-      <h2>{t("protectionData")}</h2>
-      <ul>
+    <li className="mb-4 laptop:mb-6">
+      <SectionTitle>{t("protectionData")}</SectionTitle>
+      <ul className="mb-3 laptop:mb-4">
         <li>{t("protectionDataPartOne")}</li>
         <li>{t("protectionDataPartTwo")}</li>
       </ul>
