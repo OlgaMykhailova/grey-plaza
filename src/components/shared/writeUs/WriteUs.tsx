2e0ceef8144eb1b8ca5d0cbca2f31b5b7817afc4
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-06-03 19:08:04 +0300

    updated write us section styles

--- src/components/shared/writeUs/WriteUs.tsx
+++ src/components/shared/writeUs/WriteUs.tsx
@@ -37,12 +37,12 @@ export default function WriteUs() {
         className="hidden laptop:block absolute z-[-10] min-h-[594px] w-full h-auto object-cover object-left"
       />
       <div className="h-full min-h-[792px] tablet:min-h-[614px] laptop:min-h-[594px] bg-gradient tablet:bg-writeUsGradientTablet">
-        <Container className="flex flex-col tablet:flex-row items-center tablet:items-start justify-between tablet:gap-x-4 h-full py-10">
+        <Container className="flex flex-col tablet:flex-row items-center tablet:items-start justify-between laptop:justify-around tablet:gap-x-4 h-full py-10">
           <div>
-            <SectionTitle className="tablet:w-[92.7%] text-white-text">
+            <SectionTitle className="tablet:w-[92.7%] laptop:w-[595px] mx-auto text-white-text">
               {t("subTitle")}
             </SectionTitle>
-            <p className="mb-5 text-base laptop:text-lg text-white-text text-center">
+            <p className="laptop:w-[543px] mx-auto mb-5 text-base laptop:text-lg text-white-text text-center">
               {t("leaveRequest")}
             </p>
           </div>
