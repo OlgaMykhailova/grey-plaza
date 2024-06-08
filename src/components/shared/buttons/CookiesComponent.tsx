380013901b68cd5378d9311134e66bfa952f323c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-06-08 18:02:25 +0300

    added cookies messages

--- src/components/shared/buttons/CookiesComponent.tsx
+++ src/components/shared/buttons/CookiesComponent.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+"use client";
+import { useState, useEffect, useRef } from "react";
+import Cookies from "js-cookie";
+import { useTranslations } from "next-intl";
 
 export default function CookiesComponent() {
+  const [isAccepted, setIsAccepted] = useState(true);
+  const cookiesValue = Cookies.get("isAcceptedCookies");
+  const cookiesRef = useRef<HTMLDivElement>(null);
+  const t = useTranslations("Cookies");
+
   return <div>CookiesComponent</div>;
 }
