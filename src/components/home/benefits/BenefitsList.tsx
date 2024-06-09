68a12a2d90c825987e38e9a9c9a608bd0713bca0
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-06-09 13:37:10 +0300

    added benefit section animation

--- src/components/home/benefits/BenefitsList.tsx
+++ src/components/home/benefits/BenefitsList.tsx
@@ -1,8 +1,26 @@
+"use client";
 import { useTranslations } from "next-intl";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 import BenefitsListItem from "./BenefitsListItem";
 
 export default function BenefitsList() {
   const t = useTranslations("Benefits");
+
+  const [ref, inView] = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  });
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
 
   const benefitsList = [
     {
@@ -38,10 +56,24 @@ export default function BenefitsList() {
   ];
 
   return (
-    <ul className="tablet:flex tablet:flex-wrap">
+    <motion.ul
+      initial="hidden"
+      variants={{
+        visible: {
+          transition: {
+            delayChildren: 0.4,
+            staggerChildren: 0.5,
+            ease: "easeIn",
+          },
+        },
+      }}
+      animate={controls}
+      ref={ref}
+      className="tablet:flex tablet:flex-wrap"
+    >
       {benefitsList.map((benefit, idx) => (
         <BenefitsListItem key={idx} benefit={benefit} />
       ))}
-    </ul>
+    </motion.ul>
   );
 }
--- src/components/home/benefits/BenefitsListItem.tsx
+++ src/components/home/benefits/BenefitsListItem.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Image from "next/image";
-import { InView, useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 
 interface BenefitsListItemProps {
   benefit: { title: string; description: string; icon: string };
@@ -8,14 +10,41 @@ interface BenefitsListItemProps {
 
 export default function BenefitsListItem({ benefit }: BenefitsListItemProps) {
   const { title, description, icon } = benefit;
-  const { ref, inView } = useInView({
-    threshold: 0.01,
-    rootMargin: "-45% 0% -54% 0%",
+
+  const [ref, inView] = useInView({
+    threshold: 1,
+    triggerOnce: true,
   });
+  const controls = useAnimation();
+  
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  const itemVariants = {
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        y: { stiffness: 1000, velocity: -100 },
+      },
+    },
+    hidden: {
+      y: 50,
+      opacity: 0,
+      transition: {
+        y: { stiffness: 1000 },
+      },
+    },
+  };
 
   return (
-    <li
-      ref={ref}
+    <motion.li
+      variants={itemVariants}
       className="flex gap-4 tablet:w-[46.4%] laptop:w-[33%] px-3 py-4 [&:not(:last-child)]:mb-2 tablet:[&:not(:last-child)]:mb-0 text-white-text"
     >
       <Image
@@ -23,13 +52,30 @@ export default function BenefitsListItem({ benefit }: BenefitsListItemProps) {
         width="0"
         height="0"
         alt={title}
-        className={`w-[32px] h-[32px] ${inView ? "animate-benefit" : ""}`}
+        className={`w-[32px] h-[32px]`}
       />
 
-      <div>
-        <h3 className="mb-2 text-mdb">{title}</h3>
-        <p className={`text-base`}>{description}</p>
-      </div>
-    </li>
+      <motion.div
+        animate={controls}
+        ref={ref}
+        initial="hidden"
+        variants={{
+          visible: {
+            transition: {
+              delayChildren: 0.4,
+              staggerChildren: 0.5,
+              ease: "easeIn",
+            },
+          },
+        }}
+      >
+        <motion.h3 variants={itemVariants} className="mb-2 text-mdb">
+          {title}
+        </motion.h3>
+        <motion.p variants={itemVariants} className={`text-base`}>
+          {description}
+        </motion.p>
+      </motion.div>
+    </motion.li>
   );
 }
