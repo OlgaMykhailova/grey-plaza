e2dd999d7fc6081629f5ffadc2bb316a09f45dd1
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-06-10 12:16:55 +0300

    removed buttons active effect

--- src/components/shared/buttons/TextButton.tsx
+++ src/components/shared/buttons/TextButton.tsx
@@ -16,8 +16,7 @@ export default function TextButton({
       type="button"
       onClick={onClick}
       className={`w-[320px] h-10 outline-none border-y border-primary text-baseb laptop:text-mdsb text-black laptop:hover:text-accent 
-      laptop:focus-visible:text-accent laptop:hover:border-accent laptop:focus-visible:border-accent transition-[color] 
-      duration-[300ms] ease-out-quart ${className}`}
+      laptop:focus-visible:text-accent laptop:hover:border-accent laptop:focus-visible:border-accent transition-[color] duration-[300ms] ease-out-quart ${className}`}
     >
       {children}
     </button>
