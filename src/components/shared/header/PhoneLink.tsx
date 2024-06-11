6ea7400c117bce3e54f0e85b34636837932e8dfa
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-06-11 21:29:27 +0300

    Update header phone link (#122)
    * added lazy loading components
    
    * added suspense
    
    * added suspense to footer and header menu
    
    * added motion lazy loading
    
    * updated phone link

--- src/components/shared/header/PhoneLink.tsx
+++ src/components/shared/header/PhoneLink.tsx
@@ -8,11 +8,13 @@ export default function PhoneLink() {
     <a
       href={`tel:+${t("phoneFirst").replace(/\D/g, "")}`}
       rel="noopener noreferrer nofollow"
-      className="outline-none laptop:text-md text-white-text laptop:hover:text-accent laptop:focus-visible:text-accent 
+      className="flex items-center outline-none laptop:text-md text-white-text laptop:hover:text-accent laptop:focus-visible:text-accent 
       transition-[color] duration-[300ms] ease-out-quart"
     >
-      <IconPhone className="inline w-10 h-10 laptop:w-12 laptop:h-12 p-2" />
-      <span className="hidden laptop:inline">{t("phoneFirst")}</span>
+      <div className="w-10 h-10 laptop:w-12 laptop:h-12 p-2">
+        <IconPhone />
+      </div>
+      <span className="hidden laptop:block">{t("phoneFirst")}</span>
     </a>
   );
 }
