c70daa1dee96f75469dd71c8129385cb360cafea
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-06-12 17:31:26 +0300

    updated social links styles

--- src/components/shared/socialLinks/SocialLinkItem.tsx
+++ src/components/shared/socialLinks/SocialLinkItem.tsx
@@ -13,7 +13,7 @@ export default function SocialLinkItem({ social }: SocialLinkItemProps) {
         target="_blank"
         rel="noopener noreferrer nofollow"
         aria-label={name}
-        className="relative flex items-center justify-center w-10 h-10 outline-none after:content-[''] after:absolute after:w-0 after:h-0 after:rounded-full after:bg-secondary
+        className="relative flex items-center justify-center w-[42px] h-[42px] outline-none after:content-[''] after:absolute after:w-0 after:h-0 after:rounded-full after:bg-secondary
         transition after:duration-300 after:ease-out-quart laptop:focus-visible:after:w-[38px] laptop:focus-visible:after:h-[38px] laptop:hover:after:w-[38px] laptop:hover:after:h-[38px] after:blur-[2px]"
       >
         <Image
